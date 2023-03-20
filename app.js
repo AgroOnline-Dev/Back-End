a1b298a9fdc83c30bdf673dcd6da528b42de9fe1
Noumbissi16 <noumbissistael@gmail.com>
2023-03-20 22:37:53 +0100

    merge succeful, shakur+stael backend and H

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // Importation des modules
 const express = require("express"); // Importation du module Express
-const con =require("./dbConnect")
-const cors= require("cors");
-const routesAgricult=require("./routes/agriculteursRoutes");
-const fs = require('fs');
-const http=require('http');
-const {Server}=require('socket.io')
-const path=require('path');
+const con = require("./dbConnect");
+const cors = require("cors");
+const routesAgricult = require("./routes/agriculteursRoutes");
+const fs = require("fs");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 const fileUpload = require("express-fileupload");
-
 
 // Instanciation du serveur express app
 const app = express();
@@ -18,73 +17,75 @@ const app = express();
 
 /* DEBUT PARTIE CHAT */
 //Creation serveur pour les messages
-const server=http.createServer(app);
-const io = new Server(server)
+const server = http.createServer(app);
+const io = new Server(server);
 
-io.on("connection",(socket)=>{
-  
-  console.log("Utilisateur connecté : "+socket.id)
+io.on("connection", (socket) => {
+  console.log("Utilisateur connecté : " + socket.id);
 
   //REACTION A L'EVENT REJOINDRE ROOM
-  socket.on("join-room", (room)=>{
+  socket.on("join-room", (room) => {
     socket.join(room);
-    console.log("Vous avez rejoint la room "+room)
-  })
-  
+    console.log("Vous avez rejoint la room " + room);
+  });
 
   //REACTION A L'EVENT ENVOIE DE MESSAGES
-  socket.on("send-message", (message)=>{
-    const room=message.room;
-    const user=message.userName;
-    const contenu=message.content;
-    const temps=message.time;
-    const heure=message.hour;
-    console.log("Send Message "+message.room)
-    const reqSql=" INSERT INTO messages (temps,room,user,contenu,heure) VALUES (?,?,?,?,?)";
-    con.query(reqSql,[temps,room,user,contenu,heure],(err,result)=>{
-      if (err){
-          console.log(err);      
-      }else{
-          if(result){
-               console.log("Msg saved")
-               io.to(message.room).emit("newMsgReceived", {id: new Date().getTime(), ...message})
-          }else{
-              console.log("Pas d'ingenieurs");
-          }
+  socket.on("send-message", (message) => {
+    const room = message.room;
+    const user = message.userName;
+    const contenu = message.content;
+    const temps = message.time;
+    const heure = message.hour;
+    console.log("Send Message " + message.room);
+    const reqSql =
+      " INSERT INTO messages (temps,room,user,contenu,heure) VALUES (?,?,?,?,?)";
+    con.query(reqSql, [temps, room, user, contenu, heure], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (result) {
+          console.log("Msg saved");
+          io.to(message.room).emit("newMsgReceived", {
+            id: new Date().getTime(),
+            ...message,
+          });
+        } else {
+          console.log("Pas d'ingenieurs");
+        }
       }
-   })
-  })
+    });
+  });
 
-  socket.on("disconnect",()=>{
-    console.log("Utilisateur déconnecté : "+socket.id)
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("Utilisateur déconnecté : " + socket.id);
+  });
+});
 /* FIN PARTIE CHAT */
 
 //Utilisation de cors pour faire le pont entre le domaine front et backend
 app.use(express.json());
 app.use(cors());
-app.use(fileUpload())
- 
+app.use(fileUpload());
+
 //Definition des routes des agriculteurs
 app.use(routesAgricult);
 
 //Recuperation des Ingenieurs Agronomes deja enregistres dans la BD pour les afficher sur l'appli
-app.get("/Ingenieurs",(req,res)=>{
-    const printIng="select * from agronome"
-    con.query(printIng,(err,result)=>{
-        if (err){
-            console.log(err);      
-        }else{
-            if(result){
-                 res.status(201).json(result)
-            }else{
-                console.log("Pas d'ingenieurs");
-            }
-        }
-    })
-})
- 
+app.get("/Ingenieurs", (req, res) => {
+  const printIng = "select * from agronome";
+  con.query(printIng, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        res.status(201).json(result);
+      } else {
+        console.log("Pas d'ingenieurs");
+      }
+    }
+  });
+});
+
 /*
 //INSERTION DES FICHES TECHNIQUES DANS LA BD SOUS FORMAT BLOB
  
@@ -104,36 +105,36 @@ app.get("/Ingenieurs",(req,res)=>{
  */
 
 //RECUPERATION DES FICHES SE TROUVANT DANS LA BD POUR LES AFFICHER AU NIVEAU DE L'APP
-app.get("/FichesTechniques",(req,res)=>{
-  const printFichesTech="SELECT id,nom,description FROM fichestechniques"
-  con.query(printFichesTech,(err,result)=>{
-      if (err){
-          console.log(err);      
-      }else{
-          if(result){
-            console.log(result)
-               res.status(201).json(result)
-          }else{
-              console.log("Pas de fiches techniques dans la BD");
-          }
+app.get("/FichesTechniques", (req, res) => {
+  const printFichesTech = "SELECT id,nom,description FROM fichestechniques";
+  con.query(printFichesTech, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        console.log(result);
+        res.status(201).json(result);
+      } else {
+        console.log("Pas de fiches techniques dans la BD");
       }
-  })
-})
+    }
+  });
+});
 // RENVOIT LES IMAGES DU DOSSIERS RESSOURCES EN FONCTIONS DE LEURS NOMS
-app.use(express.static(path.join(__dirname, 'Ressources')));
+app.use(express.static(path.join(__dirname, "Ressources")));
 // IMPORTATION DE MODULE
-const express = require("express");
+// const express = require("express");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 // const nodemailer = require('nodemailer');
 // const app = express();
 require("dotenv").config();
 require("express-async-errors");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
+// const cors = require("cors");
+// const fileUpload = require("express-fileupload");
 
 // const express = require("express");
-const http = require("http");
+// const http = require("http");
 
 // COMPOSANT
 const db = require("./model/db");
@@ -150,7 +151,7 @@ const deleteUsers = require("./controllers/gestionUsers/deleteUsers");
 const disconnectAdmin = require("./controllers/connectivite/disconnectAdmin");
 
 //PORT UTILISE
-const PORT = 5000 || process.env.PORT;
+const port = 5000 || process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -268,7 +269,7 @@ app.get("/seDeconnecter", verificationToken, disconnectAdmin);
 
 const router = require("./routes/agronomeRoute");
 const agriculturerRouter = require("./routes/agriculteurRoute");
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 // initialisong server with http module and not express because of socket.io
 // const server = http.createServer(app);
 
@@ -288,7 +289,7 @@ io.on("connection", (socket) => {
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 // default option for file upload
 app.use(fileUpload());
@@ -315,7 +316,6 @@ db.connect((error) => {
 // app.listen(PORT, (req, res) => {
 //     console.log(`On écoute sur le port ${PORT}`);
 // });
-   
 
 //Ecoute des requettes coté client sur le port 3001
 // serveur.listen(port,()=>{
--- dbConnect.js
+++ dbConnect.js
@@ -1,16 +1,17 @@
-const mysql= require ("mysql");
+const mysql = require("mysql");
+const dotenv = require("dotenv").config();
 
 //Connection avec la BD
- 
-   const con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password:"",
-        database:"actdb"
-    });
-    con.connect((error)=>{
-        if (error) throw error;
-        console.log("Connection a la base de donnees reussie !");
-    })
 
- module.exports=con;
+const con = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+// con.connect((error) => {
+//   if (error) throw error;
+//   console.log("Connection a la base de donnees reussie !");
+// });
+
+module.exports = con;
